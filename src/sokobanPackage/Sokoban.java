10415deb66677905bb6b5282dc597fc5424bef74
Rone <scyntherex@Rones-MacBook-Pro.local>
2017-12-06 17:13:31 -0800

    First commit. Main implemented.

---
+++ src/sokobanPackage/Sokoban.java
@@ -0,0 +1,10 @@
+package sokobanPackage;
+
+public class Sokoban {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("http://zetcode.com/tutorials/javagamestutorial/sokoban/");
+	}
+
+}
